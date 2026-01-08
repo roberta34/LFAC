165e6ca0c48d81fd45e052ec92afcbdee4dd3729
roberta34 <buzenchiroberta@gmail.com>
2026-01-08 09:35:00 +0200

    Inceput SymTable

---
+++ symbol_table.hpp
@@ -0,0 +1,58 @@
+#pragma once
+
+#include<iostream>
+#include<string>
+#include<vector>
+#include<utility>
+#include<ostream>
+#include<memory>
+
+using namespace std;
+
+struct Parameter {
+    string type;
+    string name;
+};
+
+enum class SymbolKind {
+    Variable,
+    Function,
+    Class
+};
+
+struct SymbolEntry {
+    SymbolKind kind;
+
+    string name;
+
+    string varType;
+    string value;
+
+    string returnType;
+    vector<Parameter> params;
+};
+
+class SymbolTable {
+
+    private:
+        string name; //identifica scopul si permite afisarea clara : global, clasa, functie
+        SymbolTable* parent; 
+        vector<SymbolEntry> locals;
+        vector<unique_ptr<SymbolTable>> children;
+
+        static void indent(ostream &out, int n);
+
+    public:
+        explicit SymbolTable(string scopeName, SymbolTable* parentScope);
+
+        const string& getName() const;
+        SymbolTable* getParent() const;
+
+        SymbolTable* createChild(string childName);
+        void addVariable(string& name, const string& type, const string& value="-");
+        void addFunction(const string& name, const string& returnType, const vector<Parameter>& params);
+        void addClass(const string& name);
+
+        void printAll(ostream& out, int indent=0) const;
+
+};
